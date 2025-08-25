ff6eef2ab4b306aa3279c0e9579bde92c77047fe
deepanshulodhi79-sudo <deepanshulodhi79@gmail.com>
2025-08-25 19:20:40 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Default route → login page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+// Launcher route
+app.get("/launcher", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
